d2e013163d45e0ae505258829ec77f63657342e0
kovalyov-valentin <kowalevvalen@yandex.ru>
2022-01-26 21:45:43 +0300

    check

---
+++ base_syntax_go/Go_в_примерах/2Values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")
+	fmt.Println("345 + 456 =", 345 + 456)
+	fmt.Println("8.0 / 3.0 =", 8.0 / 3.0)
+	fmt.Println("345 * 4 =", 345 * 4)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
